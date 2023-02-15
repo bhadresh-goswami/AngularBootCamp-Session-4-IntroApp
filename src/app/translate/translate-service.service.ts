b5272f1f51141986b997b8da81b4d8ded114e58c
gosai09 <gosai09@gmail.com>
2023-02-15 13:00:37 -0500

    updated translate Service

--- src/app/translate/translate-service.service.ts
+++ src/app/translate/translate-service.service.ts
@@ -3,4 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class TranslateServiceService {}
+export class TranslateServiceService {
+  constructor() {}
+  testMethod(): string {
+    return 'this.translate';
+  }
+}
